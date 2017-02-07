6f97a2e3860b850fc3cf6e2efde55bb52eb7b479
José San Gil <jsangilve@gmail.com>
2017-02-07 10:33:48 +0100

    Day 3 solution part2

--- day3/solution.ex
+++ day3/solution.ex
@@ -1,14 +1,26 @@
 defmodule Day3 do
   
-  @spec filter_input(list) :: list
-  def filter_input(data) do
+  @spec filter_by_row(list) :: list
+  defp filter_by_row(data) do
     Enum.filter(data, fn triangle -> 
-        String.split(triangle, " ", trim: true) 
+        split_row(triangle)
         |> Enum.map(&String.to_integer/1)
         |> is_triangle?
       end
     )
   end
+
+  defp split_row(triangle) when is_binary(triangle), do: String.split(triangle, " ", trim: true) 
+  defp split_row(triangle), do: triangle
+
+  @spec sort_by_col(list) :: list
+  defp sort_by_col([r1, r2, r3 | rest]) do
+    [a1, b1, c1 ] = String.split(r1, " ", trim: true)
+    [a2, b2, c2 ] = String.split(r2, " ", trim: true)
+    [a3, b3, c3 ] = String.split(r3, " ", trim: true)
+    [[a1, a2, a3], [b1, b2, b3], [c1, c2, c3]] ++ sort_by_col(rest)
+  end
+  defp sort_by_col([]), do: []
 
   @spec is_triangle?(list) :: boolean
   defp is_triangle?([a, b, c]) do
@@ -17,7 +29,15 @@ defmodule Day3 do
 
   @spec count_triangles(list) :: integer
   def count_triangles(data) do
-    length filter_input(data)
+    filter_by_row(data) |> length
+  end
+
+  
+  @spec count_col_triangles(list) :: integer
+  def count_col_triangles(data) do
+    sort_by_col(data)
+    |> filter_by_row
+    |> length
   end
 
 end
@@ -29,3 +49,4 @@ end
 
 data = File.read!(filename) |> String.split("\n", trim: true)
 IO.puts "Triangles: #{Day3.count_triangles(data)}"
+IO.puts "Vertical Triangles: #{Day3.count_col_triangles(data)}"
