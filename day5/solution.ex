8aa1f2aa7fd4d4a526a7e5ff04683355cb72b9db
José San Gil <jsangilve@gmail.com>
2017-02-19 20:14:59 +0100

    Added solution part 2

--- day5/solution.ex
+++ day5/solution.ex
@@ -1,5 +1,7 @@
 defmodule Day5 do
-  
+
+  @x_char 120
+
   @docp """
   Generate hexadecimal representation of md5 hash
   """
@@ -11,22 +13,55 @@ defmodule Day5 do
   Generate next character from door_id and index
   """
   @spec next_character(String.t, String.t, integer) :: {String.t, integer}
-  defp next_character("00000" <> xs, _,  index), do: {String.first(xs), index}
+  defp next_character("00000" <> xs, _,  index), do: {String.first(xs), String.at(xs, 1), index}
   defp next_character(_, door_id, index) do
     gen_hash(door_id <> to_string(index))
     |> next_character(door_id, index + 1)
   end
 
-  @spec calc_password(String.t) :: String.t
-  @spec calc_password(String.t, integer, String.t, integer) :: String.t
-  def calc_password(door_id, index \\ 0, acc \\ "", pwd_length \\ 0)
-  def calc_password(door_id, index, acc, pwd_length) when pwd_length < 8 do
-    {char, nindex} = next_character("", door_id, index)
+  @doc """
+  Calculate door's password from left to right
+  """
+  @spec left_right_pwd(String.t) :: String.t
+  @spec left_right_pwd(String.t, integer, String.t, integer) :: String.t
+
+  def left_right_pwd(door_id, index \\ 0, acc \\ "", len \\ 0)
+  def left_right_pwd(door_id, index, acc, pwd_length) when pwd_length < 8 do
+    {char, _, nindex} = next_character("", door_id, index)
     pwd = acc <> char
-    calc_password(door_id, nindex, pwd, String.length(pwd))
+    left_right_pwd(door_id, nindex, pwd, String.length(pwd))
   end
-  def calc_password(_, _, acc, _), do: acc
+  def left_right_pwd(_, _, acc, _), do: acc
+
+
+  @docp """
+  Insert character into 8 digit password
+  """
+  defp insert_char(acc, pos, char) when pos < 8 do
+    unless Enum.at(acc, pos) != @x_char do
+      List.replace_at(acc, pos, char)
+    else
+      acc
+    end
+  end
+  defp insert_char(acc, _, _), do: acc
+
+  @doc """
+  Calculate door's password using movie's 'hacking'
+  """
+  @spec movie_pwd(String.t, integer, list) :: list
+  def movie_pwd(door_id, index \\ 0, acc \\ 'xxxxxxxx')
+  def movie_pwd(door_id, index, acc) when is_list(acc) do
+    {position, char, nindex} = next_character("", door_id, index)
+    new_pwd = insert_char(acc, String.to_integer(position, 16), char)
+    pwd = if @x_char in new_pwd, do: new_pwd, else: to_string(new_pwd)
+    movie_pwd(door_id, nindex, pwd)
+  end
+
+  @spec movie_pwd(String.t, integer, String.t) :: String.t
+  def movie_pwd(_, _, acc), do: acc
 
 end
 
-IO.puts "Door's password is: #{ Day5.calc_password("cxdnnyjw")}"
+#IO.puts "Door's left to right password is: #{ Day5.left_right_pwd("cxdnnyjw")}"
+IO.inspect "Door's movie password is: #{ Day5.movie_pwd("cxdnnyjw")}"
